bee81f1825f40ca0950a23216d21574827e134cd
rhonjude <rhonjude007@gmail.com>
2021-03-10 15:52:33 +0530

    message

--- index.php
+++ index.php
@@ -9,6 +9,42 @@
     <title>Document</title>
 </head>
 <body>
+<nav class="navbar navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="new2.php">Soul Strings</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="new2.php">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="new.php">Concerts</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Know more
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item" href="#">Artists</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+              </li>
+            </ul>
+            <form class="d-flex">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
  <div class="container">
      <div class="row">
          <div class="col col-12 col-sm-2 col-md-2 col-lg-2 col-xl-2 col-xxl-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quibusdam molestiae nulla provident alias dicta suscipit corporis quam unde excepturi veniam eius adipisci omnis dolorum consequatur voluptates, voluptatibus non perferendis?</div>
@@ -16,5 +52,6 @@
          <div class="col col-12 col-sm-5 col-md-5 col-lg-5 col-xl-5 col-xxl-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo labore nostrum odio, consectetur facilis tenetur voluptates, deleniti reprehenderit deserunt amet sunt tempore aspernatur, corrupti recusandae nam doloribus officiis iusto totam.</div>
      </div>
  </div>
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 </body>
 </html>
